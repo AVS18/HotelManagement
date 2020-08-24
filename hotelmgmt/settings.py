82a62db4309515e482ef02b999c5e7fdbec9337c
AVS18 <avsadityavardhan18bcs@iiitkottayam.ac.in>
2020-08-25 00:18:41 +0530

    h1

--- hotelmgmt/settings.py
+++ hotelmgmt/settings.py
@@ -76,7 +76,7 @@ import dj_database_url
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
