247f915723a21b8e426a361c25e3f5012a2bba0e
A V S ADITYAVARDHAN <47914143+AVS18@users.noreply.github.com>
2020-11-19 15:51:38 +0530

    Update settings.py

--- hotelmgmt/settings.py
+++ hotelmgmt/settings.py
@@ -172,5 +172,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER='adityaintern11@gmail.com'
-EMAIL_HOST_PASSWORD='aditya12345'
+EMAIL_HOST_PASSWORD='aditya6301'
 EMAIL_USE_TLS = True 
